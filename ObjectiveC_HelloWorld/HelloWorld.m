381f6e0086e9ed1859f4d304bc9be360ce62bf13
Aditi <35162065+aditiSrivast@users.noreply.github.com>
2018-09-24 16:01:01 +0530

    Add files via upload

---
+++ ObjectiveC_HelloWorld/HelloWorld.m
@@ -0,0 +1,12 @@
+#import <objc/objc.h>
+#import <objc/Object.h>
+#import <Foundation/Foundation.h>
+ 
+@implementation TestObj
+int main()
+{
+    printf("Hello World");
+	return 0;
+}
+@end
+
---
+++ Pascal_HelloWorld/HelloWorld.pas
@@ -0,0 +1,5 @@
+
+program ideone;
+begin
+	writeln ('Hello, world.')(* your code goes here *)
+end.
