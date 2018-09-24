c004fe784ccc57027649a4454ebdb8a99723acc2
Aditi <35162065+aditiSrivast@users.noreply.github.com>
2018-09-24 16:00:07 +0530

    Add files via upload

---
+++ Perl_HelloWorld/HelloWorld.pl
@@ -0,0 +1,3 @@
+#!/usr/bin/perl
+# your code goes here
+print "Hello, World!\n";
---
+++ VB.net_HelloWorld/HelloWorld.vb
@@ -0,0 +1,12 @@
+Imports System
+
+Public Class Test
+    
+Public Shared Sub Main()
+        
+System.Console.WriteLine("Hello World ")
+    
+End Sub
+
+End Class
+
